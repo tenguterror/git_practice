821a3119fd47389c94baafcaefb42de1144b3f90
TenguTerror <vegasduard@gmail.com>
2021-06-16 16:31:40 -0600

    add logic not make sure number being used

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const addTwo = (num1, num2) => {
+    if (isNaN(num1) || isNaN(num2)) {
+        console.log('Please input a number')
+    } else {
     return num1 + num2;
+    }
 };
 
-console.log(addTwo(25, 75));
+console.log(addTwo(25, 25));
